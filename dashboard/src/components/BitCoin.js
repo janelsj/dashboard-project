9d04afcd1d303c92450c12571e228f747613c742
Jane Lim <jane_limsj@hotmail.com>
2021-10-09 18:35:52 +0800

    update

--- dashboard/src/components/BitCoin.js
+++ dashboard/src/components/BitCoin.js
@@ -1,61 +1,135 @@
-import {useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
+import Plot from 'react-plotly.js';
 import API from './API';
+// import { getJsonFromCsv } from 'convert-csv-to-json/src/index';
 
 function BitCoin(){
 
-    let [dataArray, setDataArray] = useState([]);
+    // const digitalCurrInput = '../data/digital_currency_list.csv'; 
+    // const digitalCurrOutput = '../data/digital_currency_list.json';
+
+    // const physicalCurrInput = '../data/physical_currency_list.csv'; 
+    // const physicalCurrOutput = '../data/physical_currency_list.json';
+
+    // generateJsonFileFromCsv(digitalCurrInput, digitalCurrOutput);
+    // generateJsonFileFromCsv(physicalCurrInput, physicalCurrOutput);
+    let physicalCurrArray = [];
+    // let json = getJsonFromCsv("../data/physical_currency_list.csv");
+    //     for(let i=0; i<json.length;i++) {
+    //         console.log(json[i]);
+    //     }
+
+    const [xAxis, setXAxis] = useState([]);
+    const [yAxis, setYAxis] = useState([]);
+    const [market, setMarket] = useState('SGD');
+    const [symbol, setSymbol] = useState('BTC');
 
     async function getBitCoinData() {
         return await API.get('', {
-            params: {market: 'SGD', symbol: 'BTC', function: 'DIGITAL_CURRENCY_DAILY'}
+            params: {market, symbol, function: 'DIGITAL_CURRENCY_DAILY', outputsize: 'compact'}
         });
     }
 
-    function refresh() {
-        // let array = dataArray;
-        getBitCoinData().then(response => {
-            const datesObj = response.data['Time Series (Digital Currency Daily)'];
-            let datesArray = [];
-            Object.keys(datesObj).map(item => {
-               return datesArray.push(item);
-            });
-            dataArray = datesArray.map(date => {
-                return {[date]: response.data['Time Series (Digital Currency Daily)'][date]['4a. close (SGD)']}
-            })
-                // array.push({[this.eachDate]: response.data['Time Series (Digital Currency Daily)'][this.eachDate]['4a. close (SGD)']});
-            // };
-            console.log(dataArray);
-            return setDataArray(dataArray);
-        });
-        // return setDataArray(dataArray);
+    function selection(selectElementName, arrayList) {
+        let option = document.createElement("option");
+        option.value = "";
+        option.text = "Select currency";
+        selectElementName.append(option);
+        arrayList.forEach((item) => {
+            option = document.createElement("option");
+            option.value = item;
+            option.text = item;
+            selectElementName.append(option);
+        })
+    }
+
+    function getValue(dropDownList) {
+        return dropDownList.options[dropDownList.selectedIndex].value;
     };
 
     useEffect(()=>{
-        console.log("useEffect", dataArray);
-        return () => console.log("exit", dataArray);
-    },[dataArray]);
+        // selection(document.querySelector('select[name="physicalCurrency"]'), physicalCurrOutput);
+        // selection(document.querySelector('select[name="digitalCurrency"]'), digitalCurrOutput);
+        let xValuesArray = [];
+        let yValuesArray = [];
+        getBitCoinData().then(response => {
+            for (let eachDate in response.data['Time Series (Digital Currency Daily)']){
+                console.log(response.data['Time Series (Digital Currency Daily)'][eachDate]['4a. close (SGD)']);
+                xValuesArray.push(eachDate);
+                yValuesArray.push(response.data['Time Series (Digital Currency Daily)'][eachDate]['4a. close (SGD)'])
+            }
+            setXAxis(xValuesArray);
+            setYAxis(yValuesArray);
+        });
 
-    // useEffect(()=>{
-    //     (async() => {
-    //         await API.get('', {
-    //             params: {market: 'SGD', symbol: 'BTC', function: 'DIGITAL_CURRENCY_DAILY'}
-    //         }).then(response => {
-    //             let array = dataArray;
-    //             for (eachDate in response.data['Time Series (Digital Currency Daily)']){
-    //                 array.push({[eachDate]: response.data['Time Series (Digital Currency Daily)'][eachDate]['4a. close (SGD)']});
-    //              };
-    //             setDataArray(array);
-    //         });
-    //     })()
-    //     return () => {
-    //         console.log(dataArray);
-    //     };
-    // }, [dataArray]);
+            
+            
+            // const datesObj = response.data['Time Series (Digital Currency Daily)'];
+            // let datesArray = xAxis;
+            // Object.keys(datesObj).map(item => {
+            //    return datesArray.push(item);
+            // });
+            // array = datesArray.forEach(date => {
+            //     const trace1 = {
+            //         x: [...date],
+            //         y: [...response.data['Time Series (Digital Currency Daily)'][date]['4a. close (SGD)']],
+            //         mode: 'lines+markers',
+            //         name: `${response.data['Meta Data']['2. Digital Currency Code']}`+'/'+`${response.data['Meta Data']['4. Market Code']}`,
+            //       };
+                  
+            //     const data = [trace1];
+                  
+            //     const layout = {
+            //         title: `${response.data['Meta Data']['1. Information']}`+" "+`${response.data['Meta Data']['3. Digital Currency Name']}`+"/"+`${response.data['Meta Data']['5. Market Name']}`,
+            //         xaxis: {
+            //           title: 'Time'
+            //         },
+            //         yaxis: {
+            //           title: 'SGD'
+            //         }
+            //       };
+                  
+            //     Plotly.newPlot('bitcoinGraph', data, layout);
 
+
+
+        //     array = datesArray.map(date => {
+        //         // setXAxis(date);
+        //         // setYAxis(.response.data['Time Series (Digital Currency Daily)'][date]['4a. close (SGD)']);
+        //         return {[date]: response.data['Time Series (Digital Currency Daily)'][date]['4a. close (SGD)']}
+        //     })
+        //     setXAxis(datesArray);
+        //     for (let i=0; i<array.length; i++){
+        //         setYAxis(array[i][date])
+        //     }
+        //     console.log(xAxis, yAxis);
+        //     // return setDataArray(array);
+        // });
+        return () => console.log("exit");
+    },[xAxis, yAxis, market, symbol]);
+
+
+
+    
     return(<div id="bitcoin">
-        Content for BitCoin <br/>
-        <button onClick={refresh}>Refresh</button>
-        <div id="bitcoinText">{JSON.stringify(dataArray)}</div>
+        Content for BitCoin <br/> <br/>
+        {/* <button onClick={refresh}>Refresh</button> */}
+        <label htmlFor="physicalCurrency">Choose physical currency:</label>
+        <select name="physicalCurrency"></select><br/> <br/>
+        <label htmlFor="digitalCurrency">Choose digital currency:</label>
+        <select name="digitalCurrency"></select>
+        <Plot
+            data={[
+              {
+                x: xAxis,
+                y: yAxis,
+                type: 'scatter',
+                mode: 'lines+markers',
+                marker: {color: 'red'},
+              },
+            ]}
+            layout={{width: 800, height: 600, title: `Time Series of ${symbol} (${market})`}}
+          />
     </div>)
 }
 
