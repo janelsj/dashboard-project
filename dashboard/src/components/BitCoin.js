d13d35a72fba24f40d06d3a150e05c55f12e760e
Jane Lim <jane_limsj@hotmail.com>
2021-10-12 15:39:06 +0800

    update

--- dashboard/src/components/BitCoin.js
+++ dashboard/src/components/BitCoin.js
@@ -1,84 +1,85 @@
 import React, {useState, useEffect} from 'react';
 import Plot from 'react-plotly.js';
 import API from './API';
-import { CSVReader } from 'react-papaparse';
-// import { getJsonFromCsv } from 'convert-csv-to-json/src/index';
+import Papa from 'papaparse';
 
 function BitCoin(){
 
     const [xAxis, setXAxis] = useState([]);
     const [yAxis, setYAxis] = useState([]);
-    const [market, setMarket] = useState('SGD');
-    const [symbol, setSymbol] = useState('BTC');
+    const [market, setMarket] = useState('SGD,Singapore Dollar');
+    const [symbol, setSymbol] = useState('BTC,Bitcoin');
 
     async function getBitCoinData() {
         return await API.get('', {
-            params: {market, symbol, function: 'DIGITAL_CURRENCY_DAILY', outputsize: 'compact'}
+            params: {market: market.split(",")[0], symbol: symbol.split(",")[0], function: 'DIGITAL_CURRENCY_DAILY', outputsize: 'compact'}
         });
     }
 
     /* Function to create dropdown list from array created from csv */
-    // function selection(selectElementName, arrayList) {
-    //     let option = document.createElement("option");
-    //     option.value = "";
-    //     option.text = "Select currency";
-    //     selectElementName.append(option);
-    //     arrayList.forEach((item) => {
-    //         option = document.createElement("option");
-    //         option.value = item;
-    //         option.text = item;
-    //         selectElementName.append(option);
-    //     })
-    // }
+        const selection = (selectElementName, arrayList) => {
+            let option = document.createElement("option");
+            option.value = "";
+            option.text = "Select currency";
+            selectElementName.append(option);
+            arrayList.forEach(item => {
+                option = document.createElement("option");
+                option.value = `${item['currency code']},${item['currency name']}`;
+                option.text = `${item['currency code']} (${item['currency name']})`;
+                selectElementName.append(option);
+            })
+        };
 
-    /* Function to get value of item selected from the dropdown list */
-    // function getValue(dropDownList) {
-    //     return dropDownList.options[dropDownList.selectedIndex].value;
-    // };
-
-    /* React-papaparse library */
-    // let array = Papa.parse('../data/physical_currency_list.csv', {download:true});
-    //  console.log(array)
-
-    /* converst-csv-to-json library */
-    // const digitalCurrInput = '../data/digital_currency_list.csv'; 
-    // const digitalCurrOutput = '../data/digital_currency_list.json';
-
-    // const physicalCurrInput = '../data/physical_currency_list.csv'; 
-    // const physicalCurrOutput = '../data/physical_currency_list.json';
-
-    // generateJsonFileFromCsv(digitalCurrInput, digitalCurrOutput);
-    // generateJsonFileFromCsv(physicalCurrInput, physicalCurrOutput);
-
-    // let json = getJsonFromCsv("../data/physical_currency_list.csv");
-    //     for(let i=0; i<json.length;i++) {
-    //         console.log(json[i]);
-    //     }
+    /* Function to get values for dropdown lists from csv files */
+        const getDatafromCSV = (filePathName, arrayName, DOMElementName) => {
+            Papa.parse(`/dashboard-project/data/${filePathName}_currency_list.csv`, {
+                download: true,
+                header: true,
+                step: (result) => arrayName.push(result.data),
+                complete: (results) => {
+                    results.data = arrayName;
+                    // console.log(results.data);
+                    selection(document.querySelector(`select[name=${DOMElementName}]`), results.data);
+                }
+            });
+        };
 
     useEffect(()=>{
-        // selection(document.querySelector('select[name="physicalCurrency"]'), physicalCurrOutput);
-        // selection(document.querySelector('select[name="digitalCurrency"]'), digitalCurrOutput);
+    /* Get values for dropdown lists from each csv file */
+        let physicalCurrArray = [];
+        let digitalCurrArray = [];
+        getDatafromCSV('physical', physicalCurrArray, 'physicalCurrency');
+        getDatafromCSV('digital', digitalCurrArray, 'digitalCurrency');
+
+    /* Get values for Graph from API*/
         let xValuesArray = [];
         let yValuesArray = [];
         getBitCoinData().then(response => {
-            for (let eachDate in response.data['Time Series (Digital Currency Daily)']){
-                console.log(response.data['Time Series (Digital Currency Daily)'][eachDate][`4a. close (${market})`]);
-                xValuesArray.push(eachDate);
-                yValuesArray.push(response.data['Time Series (Digital Currency Daily)'][eachDate][`4a. close (${market})`]);
+            if (response.data["Error Message"]) {
+                alert("Invalid selection. Please select another cryptocurrency.")
+            } else {
+                for (let eachDate in response.data['Time Series (Digital Currency Daily)']){
+                    // console.log(response.data['Time Series (Digital Currency Daily)'][eachDate][`4a. close (${market.split(",")[0]})`]);
+                    xValuesArray.push(eachDate);
+                    yValuesArray.push(response.data['Time Series (Digital Currency Daily)'][eachDate][`4a. close (${market.split(",")[0]})`]);
+                }
+                setXAxis(xValuesArray);
+                setYAxis(yValuesArray);
             }
-            setXAxis(xValuesArray);
-            setYAxis(yValuesArray);
         });
+
         return () => console.log("exit");
-    },[xAxis, yAxis, market, symbol]);
+
+    },[market, symbol]);
     
     return(<div id="bitcoin">
-        Content for BitCoin <br/> <br/>
-        {/* <button onClick={refresh}>Refresh</button> */}
-        <label htmlFor="physicalCurrency">Choose physical currency:</label>
-        <select name="physicalCurrency"></select><br/> <br/>
-        <label htmlFor="digitalCurrency">Choose digital currency:</label>
-        <select name="digitalCurrency"></select>
+        <h3>{'Digital \& Crypto Currency'}</h3>
+        <div className="selection">
+            <label htmlFor="physicalCurrency">Choose physical currency: </label>
+            <select name="physicalCurrency" onChange={e => setMarket(e.target.options[e.target.selectedIndex].value)}></select>
+            <label htmlFor="digitalCurrency">Choose digital currency: </label>
+            <select name="digitalCurrency" onChange={e => setSymbol(e.target.options[e.target.selectedIndex].value)}></select> 
+        </div>
         <Plot
             data={[
               {
@@ -89,7 +90,7 @@ function BitCoin(){
                 marker: {color: 'blue'},
               },
             ]}
-            layout={{width: 800, height: 600, title: `Daily Time Series of ${symbol} (${market})`}}
+            layout={{width: 800, height: 600, title: `Daily Time Series of ${symbol.split(",")[1]} valuation (in ${market.split(",")[1]})`}}
           />
     </div>)
 }
